4af17473a2f8a9e79865c5480573bacb35c16382
SHUBHAM <shubhamtiwary50@gmail.com>
2022-08-16 15:11:41 +0530

    Some minor changes

--- src/com/election/VoterImpl.java
+++ src/com/election/VoterImpl.java
@@ -1,7 +1,17 @@
 package com.election;
 
+import java.util.Scanner;
+
 public class VoterImpl {
     public static void main(String[] args) {
-
+         Voter voter = new Voter();
+         Scanner obj = new Scanner(System.in);
+        System.out.println( " Input name : ");
+        String name = obj.nextLine();
+        System.out.println(" Input age : ");
+         int age= obj.nextInt();
+         voter.setAge(age);
+         voter.setName(name);
+        System.out.println("voter.getAgeCriteria(25) = " + voter.getAgeCriteria(age));
     }
 }
