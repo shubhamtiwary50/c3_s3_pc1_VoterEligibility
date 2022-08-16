3087d7a042b50c048c0b3a18f0a5a5842e546365
SHUBHAM <shubhamtiwary50@gmail.com>
2022-08-16 14:52:19 +0530

    Completed the Voter class and created the voter impl

--- src/com/election/Voter.java
+++ src/com/election/Voter.java
@@ -3,5 +3,39 @@ package com.election;
 public class Voter {
     private int age;
     private String name;
-    static final int VOTER_ELIGIBLE_AGE=10;
+    static final int VOTER_ELIGIBLE_AGE=18;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    Voter()
+    {
+
+    }
+    public String getAgeCriteria(int age)
+    {
+       if (this.age>=18&&this.age<=120)
+       {
+           return " Eligible for voting "+name;
+       }
+       else if (this.age<18&&this.age>0)
+       {
+           return " Not eligible for voting "+name;
+       }
+       else
+           return "Age cant be negative or zero ";
+    }
 }
---
+++ src/com/election/VoterImpl.java
@@ -0,0 +1,7 @@
+package com.election;
+
+public class VoterImpl {
+    public static void main(String[] args) {
+
+    }
+}
